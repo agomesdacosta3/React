b6fb5ce4eba93fc950ff65ee6a18da787fee875a
agomesdacosta3 <caps92@live.fr>
2022-11-15 13:40:52 +0100

    Suite exo

--- jour2/src/App.js
+++ jour2/src/App.js
@@ -1,22 +1,28 @@
+import Exo1 from "./composants/exo1.jsx";
+import Menu from "./composants/menu.jsx";
 import Event from "./composants/Event";
-import Exo1 from "./composants/exo1";
-import Exo2 from "./composants/exo2";
-import Menu from "./composants/menu";
-import State from "./composants/state";
+import Exo2 from "./composants/exo2.jsx";
+import State from "./composants/state.jsx";
+import State2 from "./composants/state2.jsx";
+import Exo3 from "./composants/exo3.jsx";
+import Articles from "./composants/Articles.jsx";
 
 function App() {
   return (
     <div className="App">
-
-      <Menu />
-      <main className=" container">
-        <div className="row">
-          <Exo1 />
-          <Event />
-          <Exo2 />
-          <State />
-        </div>
-      </main>
+        <Menu />
+        <main className="container">
+          <div className="row">
+            <Exo1 />
+            <Event />
+            <Exo2 />
+            <State />
+            <State2 />
+            <Exo3 />
+            <Articles />
+          </div>
+        </main> 
+        
     </div>
   );
 }
